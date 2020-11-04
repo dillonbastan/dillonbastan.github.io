5742ddf86019ea431f5abb6f77f431cde723f059
ndivuyo <ndivuyo@gmail.com>
2020-11-04 20:28:09 +0100

    dynamic ad view

--- js/home.js
+++ js/home.js
@@ -3,36 +3,28 @@ Dillon Bastan, 2019.
 This is the JavaScript for managing the home page
 */
 
-//window.location.href = "http://www.instagram.com/dillonbastan";
+
+var scrollLock = false;
+var stopScrollThresh = 500;
 
 
-/*
-//
-var rotation = 0;
-var interval = 33;
-var degreeInc = 1;
 
-
-//
+//Jquery init
 $(document).ready( function() {
-	window.setInterval( 
-		function(){ autoRotateClass(".rotate", degreeInc, interval) }, 
-		interval 
-	);
+	$(".fakead").css({ "display": "none" });
 });
 
 
-//
-var autoRotateClass = function(className, degrees, interval) {
-	rotation += degrees;
-	$(className).rotate(rotation);
-}
 
-
-//
-jQuery.fn.roxtate = function(degrees) {
-	$(this).css({'transform' : 'rotate('+ degrees +'deg)'});
-	return $(this);
-};
-
-*/
+$(window).bind('mousewheel',function() {
+	//Start scroling
+	if (!scrollLock) {
+		$(".fakead").css({ "display": "block" });
+		scrollLock = true;
+		//Stop scrolling
+		setTimeout(function() {
+	    	$(".fakead").css({ "display": "none" });
+	        scrollLock = false;
+	    }, stopScrollThresh);
+	}
+});
